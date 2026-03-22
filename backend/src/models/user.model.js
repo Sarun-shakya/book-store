d259bbd335bbe41dce8afea9f85c17774ba63fa2
Sarun-shakya <sarunshakya414@gmail.com>
2026-03-22 20:44:20 +0545

    user model completed

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minLength: 8
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user"
+    },
+    profile: {
+        type: String
+    }
+},{timestamps: true})
+
+export const User = mongoose.model("User", userSchema);
