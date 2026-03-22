1f221f730d1199429ba8a317afdd76b2a4d75b1e
Sarun-shakya <sarunshakya414@gmail.com>
2026-03-22 15:52:17 +0545

    frontend project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="bg-green-200 text-center py-5">Frontend is running</h1>
+    </>
+  )
+}
+
+export default App
