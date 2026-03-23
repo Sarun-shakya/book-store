d12719abda7b1bfccf2be2826d438f7d8bfe91ac
Sarun-shakya <sarunshakya414@gmail.com>
2026-03-23 12:23:28 +0545

    completed order model

---
+++ backend/src/models/order.model.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const orderSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    items: [
+        {
+            book: {
+                type: Schema.Types.ObjectId,
+                ref: "Book",
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                min: 1
+            },
+            price: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+    shippingAddress: {
+        fullName: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: String,
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        postalCode: {
+            type: String
+        },
+    },
+    paymentMethod: {
+        type: String,
+        enum: ["cod", "esewa", "khalti"],
+        default: "cod"
+    },
+    paymentStatus: {
+        type: String,
+        enum: ["pending", "paid", "failed"],
+        default: "pending"
+    },
+    orderStatus: {
+        type: String,
+        enum: ["processing", "shipped", "delivered", "cancelled"],
+        default: "processing"
+    }
+}, { timestamps: true });
+
+export const Order = mongoose.model("Order", orderSchema);
