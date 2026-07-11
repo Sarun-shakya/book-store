c4e17328d1241959bcf942cf804ed7ff76d06df7
Sarun-shakya <sarunshakya414@gmail.com>
2026-07-11 21:16:41 +0545

    axios file completed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,15 +1,13 @@
-import axios from "axios";
+import axios from 'axios'
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api/v1"
-});
-
-API.interceptors.request.use((req) => {
-    const token = localStorage.getItem("token");
-    if(token){
-        req.headers.Authorization = `Bearer ${token}`;
-    }
-    return req;
+    baseURL: "http://localhost:3000/api/v1",
+    // baseURL: import.meta.env.VITE_API_URL,
+    headers:{
+        "Content-Type":"application/json",
+    },
+    withCredentials: true,
+    timeout: 10000,
 });
 
 export default API;
