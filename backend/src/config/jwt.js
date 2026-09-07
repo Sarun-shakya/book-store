f16eb299f5f86fa563f512ef4d380acaa061b4d5
Sarun-shakya <sarunshakya414@gmail.com>
2026-09-07 08:37:47 +0545

    sameSite fix for production

--- backend/src/config/jwt.js
+++ backend/src/config/jwt.js
@@ -13,7 +13,7 @@ export const generateToken = (user, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV === "production"
+        sameSite: "none",
+        secure: true
     })
 }
